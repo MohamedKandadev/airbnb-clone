3ec804cd64a223d312e480de081f063d66107b09
MohamedKandadev <kandad.dev@gmail.com>
2024-08-08 13:19:16 +0100

    error

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -1,33 +1,33 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-import prisma from '@/app/lib/prismadb';
-import getCurrentUser from "@/app/actions/getCurrentUser";
+// import prisma from '@/app/lib/prismadb';
+// import getCurrentUser from "@/app/actions/getCurrentUser";
 
-interface IParams {
-  listingId?: string;
-}
+// interface IParams {
+//   listingId?: string;
+// }
 
-export async function DELETE(
-  request: Request, 
-  { params }: { params: IParams }
-) {
-  try{
+// export async function DELETE(
+//   request: Request, 
+//   { params }: { params: IParams }
+// ) {
+//   try{
 
-    const currentUser = await getCurrentUser();
-    if(!currentUser) return NextResponse.error();
+//     const currentUser = await getCurrentUser();
+//     if(!currentUser) return NextResponse.error();
 
-    const { listingId } = params;
-    if(!listingId || typeof listingId !== 'string') throw new Error('Invalid Id')
+//     const { listingId } = params;
+//     if(!listingId || typeof listingId !== 'string') throw new Error('Invalid Id')
   
-    const listing = await prisma.listing.deleteMany({
-      where: {
-        userId: currentUser.id,
-        id: params.listingId
-      }
-    })
+//     const listing = await prisma.listing.deleteMany({
+//       where: {
+//         userId: currentUser.id,
+//         id: params.listingId
+//       }
+//     })
     
-    return NextResponse.json(listing)
-  }catch(err: any){
-    throw new Error(err)
-  }
-}
+//     return NextResponse.json(listing)
+//   }catch(err: any){
+//     throw new Error(err)
+//   }
+// }
