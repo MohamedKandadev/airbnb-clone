6e0f5c452919322ef6f05d58e01a8c1777e0cef8
MohamedKandadev <kandad.dev@gmail.com>
2024-12-12 11:54:54 +0100

    [FIXED] fixed errors

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -7,28 +7,39 @@ interface IParams {
   reservationId?: string;
 }
 
-export async function DELETE(
+export const DELETE = async (
   request: Request,
   { params }: { params: IParams }
-) {
-  const currentUser = await getCurrentUser();
+) => {
+  try {
+    const currentUser = await getCurrentUser();
 
-  if (!currentUser) {
-    return NextResponse.error();
+    if (!currentUser) {
+      return NextResponse.error();
+    }
+
+    const { reservationId } = params;
+
+    if (!reservationId || typeof reservationId !== "string") {
+      throw new Error("Invalid ID");
+    }
+
+    const reservation = await prisma.reservation.deleteMany({
+      where: {
+        id: reservationId,
+        OR: [
+          { userId: currentUser.id },
+          { listing: { userId: currentUser.id } },
+        ],
+      },
+    });
+
+    return NextResponse.json(reservation);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
-
-  const { reservationId } = params;
-
-  if (!reservationId || typeof reservationId !== "string") {
-    throw new Error("Invalid ID");
-  }
-
-  const reservation = await prisma.reservation.deleteMany({
-    where: {
-      id: reservationId,
-      OR: [{ userId: currentUser.id }, { listing: { userId: currentUser.id } }],
-    },
-  });
-
-  return NextResponse.json(reservation);
-}
+};
